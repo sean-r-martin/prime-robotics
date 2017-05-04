aa85ac4b9926024ea679f221596e007d0c3fb812
Sean Martin <srmartin777@gmail.com>
2017-05-04 11:31:54 -0400

    initial commit

--- app/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(createLogger(), thunkMiddleware));
-
-export default store;
