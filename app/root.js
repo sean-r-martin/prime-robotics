a089016a9742bb2b72d3f20bbcfd49a49483d44a
Sean Martin <srmartin777@gmail.com>
2017-05-26 16:31:24 -0400

    made the website more responsive

--- app/root.js
+++ app/root.js
@@ -1,1 +1,13 @@
 'use strict';
+
+$(document).ready(function navBarScrollLock() {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 150) {
+      $('nav').addClass('navbar-background-change');
+    }
+    if ($(window).scrollTop() < 151) {
+      $('nav').removeClass('navbar-background-change');
+    }
+  });
+
+});
--- docs/bundle.js
+++ docs/bundle.js
@@ -73,6 +73,17 @@
 "use strict";
 
 
+$(document).ready(function navBarScrollLock() {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 150) {
+      $('nav').addClass('navbar-background-change');
+    }
+    if ($(window).scrollTop() < 151) {
+      $('nav').removeClass('navbar-background-change');
+    }
+  });
+});
+
 /***/ })
 /******/ ]);
 //# sourceMappingURL=bundle.js.map
